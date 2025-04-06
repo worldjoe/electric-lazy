123c7aba7a55584d3eadb4262acaa39652b72398
Joseph Elwell <jelwell@yahoo.com>
2025-04-06 16:07:32 -0700

    Smooches on Random

--- AutoHotKey/Random.ahk
+++ AutoHotKey/Random.ahk
@@ -10,22 +10,22 @@ IfInString, Clipboard, % "Stop"
 	switch rand
 	{
 	case 1:
-		RunWait, %comspec% /c %UserProfile%\Documents\Random.bat "Fluffer Special"
+		RunWait, %comspec% /c %UserProfile%\Documents\Random.bat "Fluffer Special.m3u"
 		RunSession("FlufferSpecial.m3u", "ELL OG - Fluffer Special.PRw",0, "Fluffer.nfsprj")
 	case 2:
-		RunWait, %comspec% /c %UserProfile%\Documents\Random.bat "Star Trippin"
+		RunWait, %comspec% /c %UserProfile%\Documents\Random.bat "Star Trippin.m3u"
 		RunSession("StarTrippin.m3u", "ELL OG - Star Trippin345.PRw",0, "StarTri.nfsprj")
 	case 3:
-		RunWait, %comspec% /c %UserProfile%\Documents\Random.bat "the a bliss"
+		RunWait, %comspec% /c %UserProfile%\Documents\Random.bat "The Ablyss.m3u"
 		RunSession("Ablyss.m3u", "ELL - Ablyss789012345.PRw",0, "Ablyss1.nfsprj")
 	case 4:
-		RunWait, %comspec% /c %UserProfile%\Documents\Random.bat "Rivers Edge"
+		RunWait, %comspec% /c %UserProfile%\Documents\Random.bat "The River_s Edge.m3u"
 		RunSession("RiversEdge.m3u", "ELL - Rivers Edge2345.PRw",0, "RiversE.nfsprj")
 	case 5:
-		RunWait, %comspec% /c %UserProfile%\Documents\Random.bat "Singularity"
+		RunWait, %comspec% /c %UserProfile%\Documents\Random.bat "Singularity.m3u"
 		RunSession("Singularity.m3u", "ELL OG - Singularity2345.PRw",0, "Singula.nfsprj")
 	case 6:
-		RunWait, %comspec% /c %UserProfile%\Documents\Random.bat "Moon rise"
+		RunWait, %comspec% /c %UserProfile%\Documents\Random.bat "Moonrise.m3u"
 		RunSession("Moonrise.m3u", "ELL OG - Moonrise9012345.PRw",0, "Moonris.nfsprj")
 	}
 }
--- Random.bat
+++ Random.bat
@@ -1,30 +1,7 @@
 chcp 65001 >NUL
 rem the line above sets the batch file to unicode because oherwise the registered trademark in the soundVolumeView command below breaks
 
-rem Allow our text to speech below to play on the speakers
-%UserProfile%\Documents\nircmd setdefaultsounddevice "Speakers"
-%UserProfile%\Documents\soundvolumeview\SoundVolumeView.exe /SetDefault "2- Intel® Smart Sound Technology (Intel® SST)\Device\Speakers\Render" all "winamp.exe"
-
-rem Making the temp file
-:num
-set num=%random%
-if exist %TEMP%\temp%num%.vbs goto num
-echo ' > "%TEMP%\temp%num%.vbs"
-echo set speech = Wscript.CreateObject("SAPI.spVoice") >> "%TEMP%\temp%num%.vbs"
-echo speech.Volume = 50 >> "%TEMP%\temp%num%.vbs"
-echo speech.speak "The Electric Lazy chose %~1" >> "%TEMP%\temp%num%.vbs"
-%UserProfile%\Documents\CLeveR.exe loadplay "%UserProfile%\Documents\GodMachine.m3u"
 timeout /t 5
-start %TEMP%\temp%num%.vbs
-timeout /t 15
-del %TEMP%\temp%num%.vbs
-rem pipe all the system sounds to our fake Virtual VB speakers so we don't hear dings and such
-%UserProfile%\Documents\nircmd setdefaultsounddevice "CABLE Input"
-%UserProfile%\Documents\soundvolumeview\SoundVolumeView.exe /SetDefault "VB-Audio Virtual Cable\Device\CABLE Input\Render" 1
-rem make sure that winamp is playing to the speakers
-%UserProfile%\Documents\CLeveR.exe loadplay %UserProfile%\Documents\silence.m3u
-%UserProfile%\Documents\nircmd setappvolume winamp.exe 1 "Speakers"
-%UserProfile%\Documents\soundvolumeview\SoundVolumeView.exe /SetAppDefault "2- Intel® Smart Sound Technology (Intel® SST)\Device\Speakers\Render" all "winamp.exe"
-%UserProfile%\Documents\CLeveR.exe stop
-
+%UserProfile%\Documents\CLeveR.exe loadplay "%UserProfile%\Documents\Smooches\%~1"
+timeout /t 10
 
