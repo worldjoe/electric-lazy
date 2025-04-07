3affc98ce883b3d47eda873e2fe809b36597aafe
Joseph <jelwell@yahoo.com>
2025-04-06 18:49:08 -0700

    remove extra timeout on random

--- Random.bat
+++ Random.bat
@@ -1,7 +1,6 @@
 chcp 65001 >NUL
 rem the line above sets the batch file to unicode because oherwise the registered trademark in the soundVolumeView command below breaks
 
-timeout /t 5
 %UserProfile%\Documents\CLeveR.exe loadplay "%UserProfile%\Documents\Smooches\%~1"
 timeout /t 10
 
